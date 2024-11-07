112913a9f3bb1c516273069d807e41a532768204
wematu <simplyubiquitous2020@gmail.com>
2024-11-07 08:46:07 +0000

    entity core setup

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 global using dnet_rpg.Models;
 global using dnet_rpg.v1.Services.CharacterService;
 global using dnet_rpg.v1.DTOs.Character;
+global using Microsoft.EntityFrameworkCore;
 global using AutoMapper;
+global using dnet_rpg.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default")
+    )
+);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
